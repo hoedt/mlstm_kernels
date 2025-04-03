fix numerical issues with chunkwise triton kernel

mlstm_kernels/torch/chunkwise/triton_xl_chunk/chunkwise_gates.py
@@ -49,17 +49,16 @@ def compute_chunkwise_log_gates_vecB_vecA(
 
 @torch.compile
 def compute_chunkwise_log_gates_vecB(
-    vecF: torch.Tensor,  # (B, NH, S)
+    logF: torch.Tensor,  # (B, NH, S)
     chunk_size: int,
 ):
-    B, NH, S = vecF.shape
+    B, NH, S = logF.shape
     assert S % chunk_size == 0, f"S={S} is not divisible by chunk_size={chunk_size}"
     NC = S // chunk_size
     L = chunk_size
 
     # compute vecB
-    vecF_logsig = logsigmoid(vecF.to(dtype=torch.float32))
-    vecF_logsig_chunked = rearrange(vecF_logsig, "b nh (nc l) -> b nh nc l", nc=NC, l=L)
+    vecF_logsig_chunked = rearrange(logF, "b nh (nc l) -> b nh nc l", nc=NC, l=L)
     vecB = vecF_logsig_chunked.cumsum(dim=-1)
 
     return vecB

mlstm_kernels/torch/chunkwise/triton_xl_chunk/fw_parallel.py
@@ -61,7 +61,7 @@ def mlstm_chunkwise__parallel_fw_Hintra(
     )
 
     if siz_b_DHHV is None:
-        siz_b_DHHV = get_head_dim_block_size(head_dim=DHHV, min_block_size=128)
+        siz_b_DHHV = get_head_dim_block_size(head_dim=DHHV, min_block_size=64)
     else:
         siz_b_DHHV = siz_b_DHHV
 
@@ -80,7 +80,8 @@ def mlstm_chunkwise__parallel_fw_Hintra(
     vecN_out = torch.empty(B, NH, S, device=matQ.device, dtype=output_dtype)
     vecM_out = torch.empty(B, NH, S, device=matQ.device, dtype=output_dtype)
 
-    vecB = compute_chunkwise_log_gates_vecB(vecF=vecF, chunk_size=chunk_size)
+    logF = torch.nn.functional.logsigmoid(vecF)
+    vecB = compute_chunkwise_log_gates_vecB(logF=logF, chunk_size=chunk_size)
 
     grid = (num_b_DHHV, num_b_LQ, NC * B * NH)
     # print("grid(num_b_DHHV, num_b_LQ, NC*B*NH)", grid)
@@ -92,6 +93,7 @@ def mlstm_chunkwise__parallel_fw_Hintra(
         vecN_states=vecN_states,
         scaMinter_states=scaMinter_states,
         vecI=vecI,
+        logF=logF,
         vecB=vecB,
         matHout=matH_out,
         vecNout=vecN_out,

tests/torch/chunkwise/test_chunkwise_triton_xl_chunk.py
@@ -42,8 +42,8 @@ def test_triton_chunkwise_xl_chunk_vs_native_parallel_stablef_fp32(
         DHQK=DHQK,
         DHHV=DHHV,
         dtype=torch.float32,
-        atol_fw=2e-2,
-        rtol_fw=5e-2,
+        atol_fw=1e-2,
+        rtol_fw=1.3e-6,
         atol_fwbw=42e-1,  # we need to increase this tolerance for vecF.grad (max diff val 0.267...)
         rtol_fwbw=0.5,
         vmax=1e-3,