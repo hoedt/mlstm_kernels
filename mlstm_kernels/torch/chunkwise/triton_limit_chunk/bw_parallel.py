fix limit-chunk triton kernel gradients

mlstm_kernels/torch/chunkwise/triton_limit_chunk/bw_parallel.py
@@ -4,7 +4,7 @@
 import torch
 import triton
 
-from ....triton.chunkwise.limit_chunk import mlstm_chunkwise__parallel_bw_dQKV_kernel
+from ....triton.chunkwise.limit_chunk import mlstm_chunkwise__parallel_bw_dQKV_kernel, mlstm_chunkwise__recurrent_bw_dQKV_kernel
 from ....triton.kernel_param_heuristics import get_head_dim_block_size
 from ....utils.kernels import is_power_of_2
 from ...utils import torch2triton_dtype
@@ -21,14 +21,13 @@ def mlstm_chunkwise__parallel_bw_dQKV(
     matC_states: torch.Tensor,  # (B, NH, NC * DHQK, DHHV)
     matDeltaH: torch.Tensor,  # (B, NH, S, DHHV)
     vecL_out: torch.Tensor,  # (B, NH, S)
-    matDeltaC_states: torch.Tensor,  # (B, NH, NC * DHQK, DHHV)
     qk_scale: float = None,
     CHUNK_SIZE: int = 64,
     NUM_CHUNKS: int = 1,
     EPS: float = 1e-6,
 ) -> tuple[
-    torch.Tensor, torch.Tensor, torch.Tensor
-]:  # matDeltaQ (B,NH,S,DHQK), matDeltaK (B,NH,S,DHQK), matDeltaV (B,NH,S,DHHV)
+    torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor
+]:  # matDeltaQ (B,NH,S,DHQK), matDeltaK (B,NH,S,DHQK), matDeltaV (B,NH,S,DHHV), vecAux (B, NH, S, 1)
     B, NH, S, DHQK, DHHV = *matQ.shape, matV.shape[-1]
     NC = NUM_CHUNKS
     L = CHUNK_SIZE
@@ -56,6 +55,7 @@ def mlstm_chunkwise__parallel_bw_dQKV(
     # each b_DHQK thread block computes the contribution of its siz_b_DHQK block of matDeltaC
     # we need to sum them up to get the final result (we do this outside the kernel)
     matDeltaV = torch.zeros((num_b_DHQK, B, NH, S, DHHV), dtype=_dtype, device=_device)
+    vecAux = torch.zeros((B, NH, S, 1), dtype=_dtype, device=_device)
 
     grid = (num_b_DHQK, NC, B * NH)
     # print(f"grid: {grid}")
@@ -70,7 +70,7 @@ def mlstm_chunkwise__parallel_bw_dQKV(
         matC_states=matC_states,
         matDeltaH=matDeltaH,
         vecL_out=vecL_out,
-        matDeltaC_states=matDeltaC_states,
+        vecAux=vecAux,
         matDeltaQ=matDeltaQ,
         matDeltaK=matDeltaK,
         matDeltaV=matDeltaV,
@@ -94,9 +94,125 @@ def mlstm_chunkwise__parallel_bw_dQKV(
         str_matC_states_DHHV=matC_states.stride(3),
         str_vecL_out_B_NH=vecL_out.stride(1),
         str_vecL_out_S=vecL_out.stride(2),
+        str_vecAux_B_NH=vecAux.stride(1),
+        str_vecAux_S=vecAux.stride(2),
+        B=B,
+        NH=NH,
+        S=S,
+        DHQK=DHQK,
+        DHHV=DHHV,
+        NC=NC,
+        L=L,
+        siz_b_DHQK=siz_b_DHQK,
+        siz_b_DHHV=siz_b_DHHV,
+        DTYPE=torch2triton_dtype(_dtype),
+        EPS=EPS,
+        num_stages=num_stages,
+        num_warps=num_warps,
+    )
+    # sum up the contributions of each siz_b_DHQK block
+    matDeltaV = matDeltaV.sum(dim=0)  # (B, NH, S, DHHV)
+
+    return matDeltaQ, matDeltaK, matDeltaV, vecAux
+
+
+def mlstm_chunkwise__recurrent_bw_dQKV(
+    matK: torch.Tensor,  # (B, NH, S, DHQK)
+    matV: torch.Tensor,  # (B, NH, S, DHHV)
+    vecB: torch.Tensor,  # (B, NH, NC, L)
+    vecI: torch.Tensor,  # (B, NH, NC, L)
+    vecM_combine: torch.Tensor,  # (B, NH, S) = (B, NH, NC * L)
+    scaM_inter: torch.Tensor,  # (B, NH, NC+1)
+    vecN_states: torch.Tensor,  # (B, NH, NC * DHQK)
+    matC_states: torch.Tensor,  # (B, NH, NC * DHQK, DHHV)
+    matDeltaH: torch.Tensor,  # (B, NH, S, DHHV)
+    vecL_out: torch.Tensor,  # (B, NH, S)
+    vecAux: torch.Tensor,  # (B, NH, S, 1)
+    matDeltaC_states: torch.Tensor,  # (B, NH, NC * DHQK, DHHV)
+    vecDeltaN_states: torch.Tensor,  # (B, NH, NC * DHQK)
+    qk_scale: float = None,
+    CHUNK_SIZE: int = 64,
+    NUM_CHUNKS: int = 1,
+    EPS: float = 1e-6,
+) -> tuple[
+    torch.Tensor, torch.Tensor, torch.Tensor
+]:  # matDeltaQ (B,NH,S,DHQK), matDeltaK (B,NH,S,DHQK), matDeltaV (B,NH,S,DHHV)
+    B, NH, S, DHQK, DHHV = *matK.shape, matV.shape[-1]
+    NC = NUM_CHUNKS
+    L = CHUNK_SIZE
+    _dtype, _device = matK.dtype, matK.device
+
+    assert is_power_of_2(L), "Chunk size must be a power of 2."
+
+    if qk_scale is None:
+        qk_scale = DHQK**-0.5
+
+    siz_b_DHQK = get_head_dim_block_size(
+        head_dim=DHQK, min_block_size=32 if _dtype == torch.float32 else 64
+    )
+    siz_b_DHHV = get_head_dim_block_size(
+        head_dim=DHHV, min_block_size=32 if _dtype == torch.float32 else 64
+    )
+
+    num_b_DHQK = triton.cdiv(DHQK, siz_b_DHQK)
+
+    num_stages = 1
+    num_warps = 4 if siz_b_DHQK == 64 else 2
+
+    matDeltaQ = torch.zeros((B, NH, S, DHQK), dtype=_dtype, device=_device)
+    matDeltaK = torch.zeros((B, NH, S, DHQK), dtype=_dtype, device=_device)
+    # each b_DHQK thread block computes the contribution of its siz_b_DHQK block of matDeltaC
+    # we need to sum them up to get the final result (we do this outside the kernel)
+    matDeltaV = torch.zeros((num_b_DHQK, B, NH, S, DHHV), dtype=_dtype, device=_device)
+
+    grid = (num_b_DHQK, NC, B * NH)
+    # print(f"grid: {grid}")
+    mlstm_chunkwise__recurrent_bw_dQKV_kernel[grid](
+        matK=matK,
+        matV=matV,
+        vecB=vecB,
+        vecI=vecI,
+        vecM_combine=vecM_combine,
+        scaM_inter=scaM_inter,
+        vecN_states=vecN_states,
+        matC_states=matC_states,
+        matDeltaH=matDeltaH,
+        vecL_out=vecL_out,
+        vecAux=vecAux,
+        matDeltaC_states=matDeltaC_states,
+        vecDeltaN_states=vecDeltaN_states,
+        matDeltaQ=matDeltaQ,
+        matDeltaK=matDeltaK,
+        matDeltaV=matDeltaV,
+        qk_scale=qk_scale,
+        str_matQK_B_NH=matK.stride(1),
+        str_matQK_S=matK.stride(2),
+        str_matQK_DHQK=matK.stride(3),
+        str_matDV_num_b_DHQK=matDeltaV.stride(0),
+        str_matHV_B_NH=matV.stride(1),
+        str_matHV_S=matV.stride(2),
+        str_matHV_DHHV=matV.stride(3),
+        str_vecBI_B_NH=vecI.stride(1),
+        str_vecBI_NC=vecI.stride(2),
+        str_vecBI_L=vecI.stride(3),
+        str_vecM_combine_B_NH=vecM_combine.stride(1),
+        str_vecM_combine_S=vecM_combine.stride(2),
+        str_scaM_inter_B_NH=scaM_inter.stride(1),
+        str_scaM_inter_NC=scaM_inter.stride(2),
+        str_vecN_states_B_NH=vecN_states.stride(1),
+        str_vecN_states_NCDHQK=vecN_states.stride(2),
+        str_matC_states_B_NH=matC_states.stride(1),
+        str_matC_states_NCDHQK=matC_states.stride(2),
+        str_matC_states_DHHV=matC_states.stride(3),
+        str_vecL_out_B_NH=vecL_out.stride(1),
+        str_vecL_out_S=vecL_out.stride(2),
+        str_vecAux_B_NH=vecAux.stride(1),
+        str_vecAux_S=vecAux.stride(2),
         str_matDeltaC_states_B_NH=matDeltaC_states.stride(1),
         str_matDeltaC_states_NCDHQK=matDeltaC_states.stride(2),
         str_matDeltaC_states_DHHV=matDeltaC_states.stride(3),
+        str_vecDeltaN_states_B_NH=vecDeltaN_states.stride(1),
+        str_vecDeltaN_states_NCDHQK=vecDeltaN_states.stride(2),
         B=B,
         NH=NH,
         S=S,

mlstm_kernels/torch/chunkwise/triton_limit_chunk/bw_recurrent.py
@@ -17,12 +17,16 @@ def mlstm_chunkwise__recurrent_bw_dC(
     vecM_combine: torch.Tensor,  # (B, NH, S)
     matDeltaH: torch.Tensor,  # (B, NH, S, DHHV)
     vecL_out: torch.Tensor,  # (B, NH, S)
+    vecAux: torch.Tensor,  # (B, NH, S, 1)
     matDeltaC_last: torch.Tensor = None,  # (B, NH, DHQK, DHHV)
+    vecDeltaN_last: torch.Tensor = None,
     qk_scale: float = None,
     CHUNK_SIZE: int = 64,
     NUM_CHUNKS: int = 1,
     EPS: float = 1e-6,
-) -> torch.Tensor:  # matDeltaC_states (B, NH, (NC+1) * DHQK, DHHV)
+) -> tuple[
+    torch.Tensor, torch.Tensor
+]:  # matDeltaC_states (B, NH, (NC+1) * DHQK, DHHV), vecDeltaN_states (B, NH, (NC+1) * DHQK)
     """Computes only the deltaC gradients for the backward pass.
     The other gradients are computed in the other (kernel) function.
     We do not need to compute the gradients for the denominator, as it cancels out in the forward in the groupnorm.
@@ -38,9 +42,13 @@ def mlstm_chunkwise__recurrent_bw_dC(
         qk_scale = DHQK**-0.5
 
     USE_LAST_STATE = matDeltaC_last is not None
+    assert USE_LAST_STATE == (vecDeltaN_last is not None), "both matDeltaC and vecDeltaN must be given"
 
     matDeltaC_states = torch.zeros(
         (B, NH, (NC + 1) * DHQK, DHHV), dtype=torch.float32, device=_device
+    )
+    vecDeltaN_states = torch.zeros(
+        (B, NH, (NC + 1) * DHQK), dtype=torch.float32, device=_device
     )
 
     siz_b_DHQK = get_head_dim_block_size(head_dim=DHQK, min_block_size=64)
@@ -60,8 +68,11 @@ def mlstm_chunkwise__recurrent_bw_dC(
         vecM_combine=vecM_combine,
         matDeltaH=matDeltaH,
         vecL_out=vecL_out,
+        vecAux=vecAux,
         matDeltaC_last=matDeltaC_last,
+        vecDeltaN_last=vecDeltaN_last,
         matDeltaC_states=matDeltaC_states,
+        vecDeltaN_states=vecDeltaN_states,
         qk_scale=qk_scale,
         str_matQ_B_NH=matQ.stride(1),
         str_matQ_S=matQ.stride(2),
@@ -78,12 +89,18 @@ def mlstm_chunkwise__recurrent_bw_dC(
         str_matDeltaH_DHHV=matDeltaH.stride(3),
         str_vecL_out_B_NH=vecL_out.stride(1),
         str_vecL_out_S=vecL_out.stride(2),
+        str_vecAux_B_NH=vecAux.stride(1),
+        str_vecAux_S=vecAux.stride(2),
         str_matDeltaC_last_B_NH=matDeltaC_last.stride(1) if USE_LAST_STATE else 0,
         str_matDeltaC_last_DHQK=matDeltaC_last.stride(2) if USE_LAST_STATE else 0,
         str_matDeltaC_last_DHHV=matDeltaC_last.stride(3) if USE_LAST_STATE else 0,
+        str_vecDeltaN_last_B_NH=vecDeltaN_last.stride(1) if USE_LAST_STATE else 0,
+        str_vecDeltaN_last_DHQK=vecDeltaN_last.stride(2) if USE_LAST_STATE else 0,
         str_matDeltaC_states_B_NH=matDeltaC_states.stride(1),
         str_matDeltaC_states_NCDHQK=matDeltaC_states.stride(2),
         str_matDeltaC_states_DHHV=matDeltaC_states.stride(3),
+        str_vecDeltaN_states_B_NH=vecDeltaN_states.stride(1),
+        str_vecDeltaN_states_NCDHQK=vecDeltaN_states.stride(2),
         B=B,
         NH=NH,
         S=S,
@@ -100,4 +117,4 @@ def mlstm_chunkwise__recurrent_bw_dC(
         num_warps=num_warps,
     )
 
-    return matDeltaC_states
+    return matDeltaC_states, vecDeltaN_states